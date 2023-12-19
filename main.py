cd93c6c3ec47dc011c3e917c611c965b943ffa92
Cat-Tuong <cattuongtu3@gmail.com>
2023-12-18 16:58:04 -0800

    Re-purposing this script to scrape data from USDA.

--- main.py
+++ main.py
@@ -1,82 +1,28 @@
-import time, re, os, ssl, smtplib
-from selenium.webdriver.chrome.options import Options
-from selenium import webdriver
+import requests
 from bs4 import BeautifulSoup
-from email.message import EmailMessage
 
-MAX_PRICE = 2500
+# Get html website to scrape.
+URL = "https://snaped.fns.usda.gov/resources/nutrition-education-materials/seasonal-produce-guide"
+page = requests.get(URL)
+soup = BeautifulSoup(page.content, "html.parser")
 
-def find_first_dollar_amount(input_string):
-    # Define the regex pattern to match the dollar amount format
-    pattern = r'\$([0-9,]+)'
-
-    # Search for the pattern in the input_string
-    match = re.search(pattern, input_string)
-
-    # If a match is found, extract and return the dollar amount
-    if match:
-        dollar_amount = match.group(1)
-        # Remove commas from the dollar amount before returning
-        return int(dollar_amount.replace(',', ''))
-    else:
-        # If no match is found, return None or raise an exception, depending on your use case.
-        return None
+def get_produce(season_string):
+    season_list = []
+    elements = soup.find_all("div", class_=season_string)    
+    for element in elements:
+        unordered_list = element.find("ul", class_="usa-unstyled-list")
+        list_items = unordered_list.find_all("a")
+        for list_item in list_items:
+            season_list.append(list_item.text)
     
-def send_email(email_text):
-    # Setup SMTP server and send email notifications
-    email_sender = "cattuongtu39@gmail.com"
-    email_password = "firipfncydsrwygu"
-    email_receiver = "cattuongtu3@gmail.com"
+    return season_list
 
-    subject = 'Apartment Price Alert'
-    body = """
-    Apartment prices for Santa Fe Ranch dropped!
-    Prices Below:
+if __name__ == "__main__":
+    spring_produce = get_produce("spring")
+    summer_produce = get_produce("summer")
+    winter_produce = get_produce("winter")
+    fall_produce = get_produce("fall")
 
-    """ + email_text
-
-    em = EmailMessage()
-    em['From'] = email_sender
-    em['To'] = email_receiver
-    em['Subject'] = subject
-    em.set_content(body)
-
-    context = ssl.create_default_context()
-
-    with smtplib.SMTP_SSL('smtp.gmail.com', 465, context=context) as smtp:
-        smtp.login(email_sender, email_password)
-        smtp.sendmail(email_sender, email_receiver, em.as_string())
-
-# All prices under the max score
-prices = []
-# the following options are only for setup purposes
-chrome_options = Options()
-chrome_options.add_argument('--no-sandbox')
-chrome_options.add_argument('--disable-dev-shm-usage')
-
-driver = webdriver.Chrome(options=chrome_options)
-
-URL = "https://www.santaferanchapthomes.com/floorplans/1x1-renovated"
-
-driver.get(URL)
-time.sleep(7)  # any number > 3 should work fine
-html = driver.page_source
-f = open("index.html", "w")
-f.write(html)
-f.close()
-
-
-# Grab all rent cards and see if any of them drop under $2500
-with open("index.html") as fp:
-    soup = BeautifulSoup(fp, "html.parser")
-email_text = ""
-for tag in soup.find_all('td', class_="td-card-rent"):
-    amount = find_first_dollar_amount(tag.text)
-    if amount < MAX_PRICE:
-        email_text = email_text + "Apartment found for ${}!\n".format(amount)
-
-send_email(email_text)
         
-
 
 
